1eb5c18f7b4f6ed5bd7ca222535b4807b176706a
Sadiyya Fayaz <sadiyyafayaz@gmail.com>
2021-08-31 01:34:54 +0530

    Create main.java

---
+++ linked list/implementing/main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyLinkedlist l=new MyLinkedlist();
+        l.add(12);
+        l.add(10);
+        l.print();
+        
+    }
+}
