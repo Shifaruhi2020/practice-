03ed054a04caadf820be7c63f28945ea3ede80e9
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2025-04-04 20:43:04 +0530

    Time: 15 ms (28.98%) | Memory: 48.1 MB (36.61%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null && strs.length == 0){
+            return new ArrayList<>();
+        }
+        Map<String, List<String> > freqMap = new HashMap<>();
+        for(String str : strs){
+            String freqString = getFreqString(str);
+
+            if(freqMap.containsKey(freqString)){
+                freqMap.get(freqString).add(str);
+            }
+            else{
+                List<String> newList = new ArrayList<>();
+                newList.add(str);
+                freqMap.put(freqString,newList);
+            } 
+        }
+        return new ArrayList<>(freqMap.values());  
+    }
+
+    private String getFreqString(String str){
+        int[] freq = new int[26];
+        for (char c : str.toCharArray()){
+            freq[c - 'a']++;
+        }
+        StringBuilder newFreqString = new StringBuilder("");
+        // System.out.println(newFreqString instanceof StringBuilder);
+        char c = 'a';
+        for(int i : freq){
+            newFreqString.append(c);
+            newFreqString.append(i);
+            c++;
+        }
+        return newFreqString.toString();
+    }
+
+}
