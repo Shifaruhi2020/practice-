544e092c0a814092f5dd6bd3731fef463891ffc1
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2025-05-06 19:25:51 +0530

    updated code for valid anagram>array>leetcode>easy

--- Array/Leetcode/Easy/242-valid-anagram/valid-anagram.java
+++ Array/Leetcode/Easy/242-valid-anagram/valid-anagram.java
@@ -1,27 +1,25 @@
-import java.util.Arrays;
+
+// solution link: https://www.youtube.com/watch?v=IRN1VcA8CGc
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        String str1 = s.toLowerCase();
-        String str2 = t.toLowerCase();
+    int[] char_count = new int[26];
 
-        str1 = str1.replace(" ","");
-        str2 = str2.replace(" ","");
-        
-        int[] counts = new int[26];
-
-        for (int i =0; i < str1.length(); i ++){
-            counts[str1.charAt(i) - 'a']++ ;
+        if(s.length() == t.length()){
+                   for(int i = 0; i < s.length(); i++ ){
+            char_count[s.charAt(i) - 'a']++;
+            char_count[t.charAt(i) - 'a']--;
         }
 
-        for (int i =0; i < str2.length(); i ++){
-            counts[str2.charAt(i) - 'a']-- ;
+        for(int count:char_count){
+            if(count != 0){
+                return false;
+            }
         }
-
-        for(int count : counts){
-            if(count != 0) 
-                return false; 
-        }  
-        return true;  
+        return true;
+        }
+        else{
+            return false;
+        }
     }
 }
