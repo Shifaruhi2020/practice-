bebf48e122f80580a1b376d3576bd320ee614f6c
Sadiyya Fayaz <sadiyyafayaz@gmail.com>
2025-05-20 22:04:42 +0530

    Update two-sum.java

--- Array/Leetcode/Easy/1-two-sum/two-sum.java
+++ Array/Leetcode/Easy/1-two-sum/two-sum.java
@@ -1,16 +1,15 @@
 // solution link : https://www.youtube.com/watch?v=7jDS9KQEDbI
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> table = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++){
-            int complement = target - nums[i];
-
-            if (table.containsKey(complement) ){
-                return new int[] {table.get(complement), i};
+        HashMap<Integer, Integer> value_index = new HashMap<>();
+        int[] ans = new int[2];
+        for (int i =0; i < nums.length ; i ++){
+            int val = target - nums[i];
+            if(value_index.containsKey(val)){
+                return new int[] {value_index.get(val), i};
             }
-            table.put(nums[i], i);
-        }    
-        throw new IllegalArgumentException("No Two Sum solution");  
+            value_index.put(nums[i], i);
+        } 
+        return new int[] {};
     }
 }
