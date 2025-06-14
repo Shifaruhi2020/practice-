54041f956cd3d85aced51db480824f6c4e561b98
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2025-06-14 14:24:52 +0530

    updated reverse Linked List

--- LinkedList/LeetCode/Easy/206-reverse-linked-list/reverse-linked-list.java
+++ LinkedList/LeetCode/Easy/206-reverse-linked-list/reverse-linked-list.java
@@ -8,18 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-//  link solution : https://www.youtube.com/watch?v=D2vI2DNJGd8
+//  link solution : https://www.youtube.com/watch?v=3IN0BP9Ni6E
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode cur = head;
-        while(cur != null){
-            ListNode tmp = cur.next;
-            cur.next = prev;
-            prev = cur; 
-            cur = tmp;
+        if(head == null){
+            return null;
         }
-        return prev;
-        
+        if(head.next == null){
+            return head;
+        }
+
+        ListNode preNode = null;
+        ListNode currNode = head;
+
+        while(currNode != null ){
+            ListNode nextNode = currNode.next;
+            currNode.next = preNode;
+            preNode = currNode;
+            currNode = nextNode;
+        }
+        head = preNode;
+        return head;
     }
 }
