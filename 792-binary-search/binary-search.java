5efd8fd6a373b85e2c867ccc2b32f61b9ad79b00
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2025-07-31 00:44:01 +0530

    new changes in the BinarySearch code

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,7 +2,11 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-
+       
+    //    nums = [5], target = 5
+    // in this case if we do left < right instead of left<= right
+    // then returns -1 instead of 0
+    // left <= right -> this condition helps with, single item array
         while(left <= right){
             int mid = left+ (right - left) / 2;
             if(nums[mid] == target){
