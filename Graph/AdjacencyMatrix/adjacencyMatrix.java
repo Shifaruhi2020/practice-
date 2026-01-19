411dce72b963a8eeba2a09e4911cab600f46e834
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2026-01-19 17:36:11 +0530

    adjacencyMatrix -> Graph

---
+++ Graph/AdjacencyMatrix/adjacencyMatrix.java
@@ -0,0 +1,38 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class Graph {
+    
+    private int vertices;
+    private int[][] adjMatrix;
+    
+    Graph(int vertices){
+        this.vertices = vertices;
+        adjMatrix = new int[vertices][vertices];
+    }
+    
+    void addEdge(int u, int v){
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;
+    }
+    
+    void printGraph(){
+        for(int i = 0; i < vertices; i++){
+            for(int j = 0; j < vertices; j++){
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+          System.out.println();  
+        }
+        
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        
+        g.printGraph();
+        
+    }
+}
