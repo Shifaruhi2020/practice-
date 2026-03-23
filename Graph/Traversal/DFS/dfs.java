5f2dc1e636b1488034e3bc1fd599ea20e87b7e8d
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2026-03-23 16:37:58 +0530

    Traversal -> DFS

---
+++ Graph/Traversal/DFS/dfs.java
@@ -0,0 +1,66 @@
+// vid link : https://www.youtube.com/watch?v=-tgVpUgsQ5k
+
+import java.util.*;
+
+class Main {
+    
+    public static void dfs(int V, ArrayList<ArrayList<Integer>> adj, int src){
+        
+        int [] visited = new int[V];
+        
+        dfsRecursive(src, adj, visited);
+    }
+    
+    public static void dfsRecursive(int node, ArrayList<ArrayList<Integer>> adj, int[] visited){
+        
+        visited[node] = 1;
+        System.out.println(node + " ");
+        
+        for(int neighbour: adj.get(node)){
+            if(visited[neighbour] == 0){
+                dfsRecursive(neighbour, adj, visited);
+            }
+        }
+    }
+    
+    public static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v){
+        
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+    
+    public static void main(String[] args) {
+        
+        int V = 9;
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i =0; i < V; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        addEdge(adj,1, 2);
+        addEdge(adj,1, 3);
+        addEdge(adj,2, 5);
+        addEdge(adj,2, 6);
+        addEdge(adj,3, 4);
+        addEdge(adj,3, 7);
+        addEdge(adj,4, 8);
+        addEdge(adj,7, 8);
+        
+        System.out.println("DFS traversal: ");
+        dfs(V, adj, 1);
+    }
+}
+
+
+// output
+// BFS Traversal: 1 
+// 2 
+// 6 
+// 3 
+// 4 
+// 7 
+// 9 
+// 5 
+// 8
