0068d3386c536d191b8c4666536e949f2dfccd3f
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2026-04-07 19:34:31 +0530

    dfs code with just one dfs method also added in the comment

--- Graph/Traversal/DFS/dfs.java
+++ Graph/Traversal/DFS/dfs.java
@@ -64,3 +64,80 @@ class Main {
 // 9 
 // 5 
 // 8
+
+
+
+// code if we want to combine the two dfs methods
+
+// to combine the two methods we will have pass the visited array in the method so that the state of visited doesnt
+// change with every recursion
+
+// import java.util.*;
+
+// class Main {
+    
+//     public static void dfs(int node, ArrayList<ArrayList<Integer>> adj, int[] visited){
+        
+//         visited[node] = 1;
+//         System.out.print(node + " ");
+        
+//         for(int neighbour : adj.get(node)){
+//             if(visited[neighbour] == 0){
+//                 dfs(neighbour, adj, visited);
+//             }
+//         }
+//     }
+
+// keep this code commented
+    
+    // public static void dfsRecursive(int node, ArrayList<ArrayList<Integer>> adj, int[] visited){
+        
+    //     visited[node] = 1;
+    //     System.out.println(node + " ");
+        
+    //     for(int neighbour: adj.get(node)){
+    //         if(visited[neighbour] == 0){
+    //             dfsRecursive(neighbour, adj, visited);
+    //         }
+    //     }
+    // }
+    
+//  till here 
+// kept the above code just for the purpose of understanding
+
+
+//     public static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v){
+        
+//         adj.get(u).add(v);
+//         adj.get(v).add(u);
+//     }
+    
+//     public static void main(String[] args) {
+        
+//         int V = 9;
+        
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+//         for(int i = 0; i < V; i++){
+//             adj.add(new ArrayList<>());
+//         }
+        
+//         addEdge(adj, 1, 2);
+//         addEdge(adj, 1, 3);
+//         addEdge(adj, 2, 5);
+//         addEdge(adj, 2, 6);
+//         addEdge(adj, 3, 4);
+//         addEdge(adj, 3, 7);
+//         addEdge(adj, 4, 8);
+//         addEdge(adj, 7, 8);
+        
+//         System.out.println("DFS Traversal:");
+        
+//         int[] visited = new int[V];   // ✅ created ONCE
+//         dfs(1, adj, visited);         // ✅ passed to recursion
+//     }
+// }
+
+// output
+// DFS Traversal:
+// 1 2 5 6 3 4 8 7 
