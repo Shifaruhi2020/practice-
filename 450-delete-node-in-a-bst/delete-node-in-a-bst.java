7572fb9064e023c5d942a00d3242d315391fb80b
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2026-09-01 23:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (18.45%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,85 @@
+// solution link : https://www.youtube.com/watch?v=kouxiP_H5WE
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return null;
+        }
+
+        if(root.val == key){
+            return helper(root);
+        }
+
+        TreeNode dummy = root;
+
+        while(root != null){
+            if(root.val > key){
+
+                if(root.left != null && root.left.val == key){
+                    root.left = helper(root.left);
+                    break;
+                }
+                else{
+                    root = root.left;
+                }
+
+            }
+
+            else{
+
+                if(root.right != null && root.right.val == key){
+                    root.right = helper(root.right);
+                    break;
+                }
+                else{
+                    root = root.right;
+                }
+            }
+        }
+
+        return dummy;
+    }
+
+    public TreeNode helper(TreeNode root){
+
+        if(root.left == null){
+            return root.right;
+        }
+        else if(root.right == null){
+            return root.left;
+        }
+
+        else{
+
+            TreeNode rightChild = root.right;
+            TreeNode lastRight = lastRightChild(root.left);
+            lastRight.right = rightChild;
+
+            return root.left;
+        }
+
+    }
+
+    public TreeNode lastRightChild(TreeNode root){
+
+        if(root.right == null){
+            return root;
+        }
+        return lastRightChild(root.right);
+
+    }
+}
